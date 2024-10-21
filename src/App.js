2b96cd58f17dbcc30d5c7a917346d1a4ed995915
sandesh300 <bhujbalsandesh52@gmail.com>
2024-10-21 23:48:20 +0530

    init rule engine react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import CreateRule from './pages/CreateRule';
+import ListRules from './pages/ListRules';
+import CombineRules from './pages/CombineRules';
+import EvaluateRule from './pages/EvaluateRule';
+import ModifyRule from './pages/ModifyRule';
+import './index.css'; // Add your custom styles
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Rule Engine</h1>
+            <CreateRule />
+            <ListRules />
+            <CombineRules />
+            <EvaluateRule />
+            <ModifyRule />
+        </div>
+    );
 }
 
 export default App;
