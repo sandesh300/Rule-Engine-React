8158f6d448ffcc44c809fcf70eca22e8fb1b971f
sandesh300 <bhujbalsandesh52@gmail.com>
2024-10-22 23:15:37 +0530

    add the routes of app pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-// src/App.js
 import React from 'react';
-import CreateRule from './pages/CreateRule';
-import ListRules from './pages/ListRules';
-import CombineRules from './pages/CombineRules';
-import EvaluateRule from './pages/EvaluateRule';
-import ModifyRule from './pages/ModifyRule';
-import './index.css'; // Add your custom styles
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar';
+import CreateRule from './components/CreateRule';
+import CombineRule from './components/CombineRule';
+import EvaluateRule from './components/EvaluateRule';
+import ModifyRule from './components/ModifyRule';
+import Home from './components/Home';
+
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Rule Engine</h1>
-            <CreateRule />
-            <ListRules />
-            <CombineRules />
-            <EvaluateRule />
-            <ModifyRule />
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <div className="container mx-auto px-4 py-8">
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+            <Route path="/create" element={<CreateRule />} />
+            <Route path="/combine" element={<CombineRule />} />
+            <Route path="/evaluate" element={<EvaluateRule />} />
+            <Route path="/modify" element={<ModifyRule />} />
+          </Routes>
         </div>
-    );
+        <ToastContainer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
